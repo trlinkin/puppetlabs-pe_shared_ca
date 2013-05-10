b34b9298222bc04df3a8ee6c7c3927bf93721150
Hunter Haugen <h.haugen@gmail.com>
2013-05-10 11:24:49 -0700

    Update params for pe-internal-broker and ssldir

--- manifests/params.pp
+++ manifests/params.pp
@@ -1,17 +1,19 @@
 class pe_shared_ca::params {
-  $ssldir = $settings::ssldir
 
   $ca_files_to_copy = [
     "ca/ca_crl.pem",
     "ca/ca_crt.pem",
     "ca/ca_key.pem",
     "ca/ca_pub.pem",
+    "certs/pe-internal-broker.pem",
     "certs/pe-internal-mcollective-servers.pem",
     "certs/pe-internal-peadmin-mcollective-client.pem",
     "certs/pe-internal-puppet-console-mcollective-client.pem",
+    "private_keys/pe-internal-broker.pem",
     "private_keys/pe-internal-mcollective-servers.pem",
     "private_keys/pe-internal-peadmin-mcollective-client.pem",
     "private_keys/pe-internal-puppet-console-mcollective-client.pem",
+    "public_keys/pe-internal-broker.pem",
     "public_keys/pe-internal-mcollective-servers.pem",
     "public_keys/pe-internal-peadmin-mcollective-client.pem",
     "public_keys/pe-internal-puppet-console-mcollective-client.pem",
@@ -20,11 +22,11 @@ class pe_shared_ca::params {
 
   # Setup variables to represent various files this class will manipulate
   $ca_files_to_purge = [
-    "${ssldir}/certs/ca.pem",
-    "${ssldir}/certs/${::clientcert}.pem",
-    "${ssldir}/private_keys/${::clientcert}.pem",
-    "${ssldir}/public_keys/${::clientcert}.pem",
-    "${ssldir}/crl.pem",
+    "/etc/puppetlabs/puppet/ssl/certs/ca.pem",
+    "/etc/puppetlabs/puppet/ssl/certs/${::clientcert}.pem",
+    "/etc/puppetlabs/puppet/ssl/private_keys/${::clientcert}.pem",
+    "/etc/puppetlabs/puppet/ssl/public_keys/${::clientcert}.pem",
+    "/etc/puppetlabs/puppet/ssl/crl.pem",
   ]
   $mco_files_to_purge = [
     "/etc/puppetlabs/mcollective/ssl",
